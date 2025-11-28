8317213ed8c8495d2861c8b606961dbef41c8e53
Contreras <johans_contreras@soy.sena.edu.coo>
2025-11-28 08:03:55 -0500

    feat: Agregar modelos padre (Administrador, Cliente, Marca, Proveedor)

--- proyecto_3/app/models.py
+++ proyecto_3/app/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Administrador(models.Model):
+    nombre = models.CharField(max_length=150, verbose_name="Nombre")
+    usuario = models.CharField(max_length=50, unique=True, verbose_name="Usuario")
+    contrasena = models.CharField(max_length=255, verbose_name="Contraseña")
+    email = models.EmailField(max_length=100, verbose_name="Email")
+    fechaRegistro = models.DateField(default=datetime.now, verbose_name="Fecha de Registro")
+    
+    def __str__(self):
+        return str(self.id)
+    
+    class Meta:
+        verbose_name = "administrador"
+        verbose_name_plural = "administradores"
+        db_table = "administrador"
+
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=150, verbose_name="Nombre")
+    telefono = models.CharField(max_length=20, verbose_name="Teléfono")
+    email = models.EmailField(max_length=100, verbose_name="Email")
+    fechaCasastro = models.DateField(default=datetime.now, verbose_name="Fecha de Registro")
+    
+    def __str__(self):
+        return self.id
+    
+    class Meta:
+        verbose_name = "cliente"
+        verbose_name_plural = "clientes"
+        db_table = "cliente"
+
+
+class Marca(models.Model):
+    nombreMarca = models.CharField(max_length=100, verbose_name="Nombre Marca")
+    
+    def __str__(self):
+        return self.id
+    
+    class Meta:
+        verbose_name = "marca"
+        verbose_name_plural = "marcas"
+        db_table = "marca"
+
+
+class Proveedor(models.Model):
+    nombre = models.CharField(max_length=150, verbose_name="Nombre")
+    telefono = models.CharField(max_length=20, verbose_name="Teléfono")
+    email = models.EmailField(max_length=100, verbose_name="Email")
+    envio = models.IntegerField(default=1, verbose_name="Envío")
+    fechaRegistro = models.DateField(default=datetime.now, verbose_name="Fecha de Registro")
+    
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name = "proveedor"
+        verbose_name_plural = "proveedores"
+        db_table = "proveedor"
