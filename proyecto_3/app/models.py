195a23e57d3a5b08d62e421d54e60ac8b7f25fd2
Juan Ricardo Gutierrez Gonzalez <juan.gutierrez035@soy.sena.edu.co>
2025-11-28 08:56:35 -0500

    Actualizo modelo Pedidos en models.py

--- proyecto_3/app/models.py
+++ proyecto_3/app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.  
+
+#modelo pedido 
+class Pedidos(models.Model):
+    id_administrador = models.ForeignKey('administrador.Administradores', on_delete=models.CASCADE)
+    id_cliente = models.ForeignKey('cliente.Clientes', on_delete=models.CASCADE)
+    fecha_pedido = models.DateField()
+    estado_pedido = models.CharField(max_length=150)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return f"Pedido {self.id} - {self.estado_pedido} - total: {self.total}"
+    
+    
+    class Meta:
+        verbose_name = "Pedido"
+        verbose_name_plural = "Pedidos"
+        ordering = ['-fecha_pedido']
+        
+        def __str__(self):
+            return f"Pedido {self.id} - {self.estado_pedido} - total: {self.total}"
+        
+            
+        
+        
+            
+        
+    
+    
+
+        
+    
+    
